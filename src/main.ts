b56d20d37d8099714b58933141b0d3d3282c347e
Shadab Alam <shadabalamcse0@gmail.com>
2025-11-05 14:17:57 +0530

    AngularTemplates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+import { Component } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  template:`
+  <h3>Hellow {{name}}</h3>
+  <button (click)="name = 'Angular'">Reset</button>`
+})
+
+export class App{
+  name = 'World';
+}
+
+bootstrapApplication(App)
   .catch((err) => console.error(err));
